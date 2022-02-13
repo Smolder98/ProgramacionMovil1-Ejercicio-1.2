8663f5ef72e82e3a03a46cd008a3f706fa28d7cb
Smolder98 <alicoescobar@gmail.com>
2022-02-13 17:35:37 -0600

    ultimos cambios

--- app/src/main/java/com/aplicacion/ejercicio_12/MainActivity.java
+++ app/src/main/java/com/aplicacion/ejercicio_12/MainActivity.java
@@ -41,10 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnenviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(true){
-
-                    // Esto solo lo hago para probar el Bundle
-
+                if(permitirEnviar()){
                     try {
 
                         nombres = txtnombres.getText().toString();
@@ -76,23 +73,80 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private boolean validarCorreo(){
+    private boolean permitirEnviar(){
+        String mensaje = "";
+
+        String nombres = txtnombres.getText().toString();
+        String apellidos = txtapellidos.getText().toString();
+        String correo = txtcorreo.getText().toString();
+        String edad = txtedad.getText().toString();
+
+        if(isTextEmpty(nombres)) mensaje = "El campo nombres esta vacio";
+        else if(isTextEmpty(apellidos)) mensaje = "El campo apellidos esta vacio";
+        else if(isTextEmpty(edad)) mensaje = "El campo edad esta vacio";
+        else if(isTextEmpty(correo)) mensaje = "El campo correo esta vacio";
+        else if(!isNumeric(edad)) mensaje = "La edad debe ser numerica";
+        else if(!validarCorreo(correo)) mensaje = "Correo no valido";
+        else if(!isText(nombres)) mensaje = "Los nombres no son validos: Solo deben ser letras";
+        else if(!isText(apellidos)) mensaje = "Los apellidos no son validos: Solo deben ser letras";
+
+
+        if(mensaje.length() != 0){
+            Toast.makeText(getApplicationContext(), "Error al procesar datos: " + mensaje, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    /******************************************************************/
+
+    //Si el correo es valido
+    private boolean validarCorreo(String email){
         Pattern pattern = Pattern
                 .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 
         // El email a validar
-        String email = txtcorreo.getText().toString();
+//        String email = txtcorreo.getText().toString();
 
         Matcher mather = pattern.matcher(email);
-
-
 
         if(mather.find()) {
             return true;
         }else{
-            Toast.makeText(this, "El correo ingresado no es valido: " + email, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "El correo ingresado no es valido: " + email, Toast.LENGTH_SHORT).show();
             return false;
         }
 
     }
+    private static boolean isNumeric(String cadena){
+        try {
+            Double.parseDouble(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+
+    private static boolean isText(String text){
+
+        // Validando un texto que solo acepte letras sin importar tamaño
+        Pattern pat = Pattern.compile("^[a-zA-ZáéíóúÁÉÓÚÍ ]+$");
+        Matcher mat = pat.matcher(text);
+
+        return (mat.matches());
+//        return (mat.matches())?true:false;
+
+    }
+
+
+    //Si el texto esta vacio
+    private static boolean isTextEmpty(String text){
+
+        return (text.length()==0)?true:false;
+    }
+
+
 }
